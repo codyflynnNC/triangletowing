71f4ae45224dbcb36646cc8c61db681d776189da
Cody Flynn <CodyFlynn2016@gmail.com>
2016-08-12 10:11:48 -0400

    6

---
+++ js/contact.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" id="alertButton" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $("#submitButton").css("display", "none");
+                        $('.successMessages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 $(function() {
   $("#header").load("header.html");
+  $("#testphp").load("test.php");
 });
 
 $(document).ready(function() {
+  $(".content").on("click", "#alertButton", function() {
+  console.log("we clicked the alert");
+  $("#submitButton").css("display", "inline-block");
+  })
 
   var $objHead = $( 'head' );
 
@@ -27,7 +32,7 @@ $(document).ready(function() {
    }
 
 
-  $('#contact_form').bootstrapValidator({
+  $('#contact-form').bootstrapValidator({
       // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
       feedbackIcons: {
           valid: 'glyphicon glyphicon-ok',
@@ -91,23 +96,5 @@ $(document).ready(function() {
                   }
               }
           }
-      })
-      .on('success.form.bv', function(e) {
-          $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-              $('#contact_form').data('bootstrapValidator').resetForm();
-
-          // Prevent form submission
-          e.preventDefault();
-
-          // Get the form instance
-          var $form = $(e.target);
-
-          // Get the BootstrapValidator instance
-          var bv = $form.data('bootstrapValidator');
-
-          // Use Ajax to submit form data
-          $.post($form.attr('action'), $form.serialize(), function(result) {
-              console.log(result);
-          }, 'json');
+       })
       });
-});
